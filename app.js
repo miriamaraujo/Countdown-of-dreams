88e57f51eeee208109b7d81ff6dae092317a7b0f
Miriam Silva Araujo <miriam.design@live.com>
2021-08-14 22:35:52 +0100

    Dark mode added
    I have added the dark mode using the library Anime.Js to create a timeline of events called by a click event and a toggle function

--- app.js
+++ app.js
@@ -22,3 +22,48 @@ const countdown = () => {
 }
 
 setInterval(countdown, 1000);
+
+// Dark mode
+
+const moonPath =
+    "M29 59C29 91.5848 59 118 59 118C26.4152 118 0 91.5848 0 59C0 26.4152 26.4152 0 59 0C59 0 29 26.4152 29 59Z";
+const sunPath =
+    "M118 59C118 91.5848 91.5848 118 59 118C26.4152 118 0 91.5848 0 59C0 26.4152 26.4152 0 59 0C91.5848 0 118 26.4152 118 59Z";
+const darkMode = document.querySelector('#dark-mode');
+let toggle = false;
+
+darkMode.addEventListener('click', () => {
+    const timeline = anime.timeline({
+        duration: 750,
+        easing: "easeOutExpo"
+    });
+
+    timeline.add({
+            targets: ".sun",
+            d: [{
+                value: toggle ? sunPath : moonPath
+            }]
+        })
+        .add({
+            targets: ".sun",
+            // rotate: 365
+
+        }, "-= 600").add({
+            targets: "section",
+            backgroundColor: toggle ? "rgb(234, 235, 241)" : "rgb(21, 26, 32)",
+        }, "-= 600").add({
+            targets: ".day, .hour, .minute,.second",
+            backgroundColor: toggle ? "rgb(255, 255, 255)" : "rgb(29, 35, 43)",
+            color: toggle ? "rgb(56, 67, 78)" : "rgb(197, 206, 218)"
+
+        }, "-= 600").add({
+            targets: "section, h2",
+            color: toggle ? "rgb(56, 67, 78)" : "rgb(197, 206, 218)"
+
+        }, "-= 600");
+    if (!toggle) {
+        toggle = true;
+    } else {
+        toggle = false;
+    }
+});
